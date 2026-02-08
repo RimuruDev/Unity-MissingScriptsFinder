9d55731a2088be0ec2ffa0788b93d98ccf4710c0
RimuruDev <Rimuru.Dev@gmail.com>
2026-02-09 01:50:51 +0400

    Fix Unity 2020 compatibility: proper UPM manifest and editor-only assembly

--- Editor/MissingScriptsFinder.cs
+++ Editor/MissingScriptsFinder.cs
@@ -10,21 +10,23 @@
 //
 // **************************************************************** //
 
-#if !UNITY_2020_OR_NEWER && !UNITY_2020_1_OR_NEWER
-using System.Linq;
-#endif
-
+#if UNITY_EDITOR
+using System.Collections.Generic;
 using UnityEditor;
-using UnityEngine;
 using UnityEditor.SceneManagement;
+using UnityEngine;
 
 namespace AbyssMoth
 {
     public sealed class MissingScriptsFinder : EditorWindow
     {
         [MenuItem("RimuruDev Tools/Find Missing Scripts")]
-        public static void ShowWindow() =>
-            GetWindow<MissingScriptsFinder>("Find Missing Scripts");
+        public static void ShowWindow()
+        {
+            var window = GetWindow<MissingScriptsFinder>();
+            window.titleContent = new GUIContent("Find Missing Scripts");
+            window.Show();
+        }
 
         private void OnGUI()
         {
@@ -38,106 +40,167 @@ namespace AbyssMoth
                 FindMissingScriptsInPrefabs();
         }
 
-        private static GameObject[] FindGameObjects(bool includeInactive = true)
+        private static Transform[] FindSceneTransforms(bool includeInactive)
         {
 #if UNITY_6000_0_OR_NEWER
-            var inactiveMode = includeInactive
-                ? FindObjectsInactive.Include
-                : FindObjectsInactive.Exclude;
-
-            return Object.FindObjectsByType<GameObject>(inactiveMode, FindObjectsSortMode.InstanceID);
-#elif UNITY_2020_OR_NEWER || UNITY_2020_1_OR_NEWER
-            return Object.FindObjectsOfType<GameObject>(includeInactive);
+            var inactiveMode = includeInactive ? FindObjectsInactive.Include : FindObjectsInactive.Exclude;
+            return FindObjectsByType<Transform>(inactiveMode, FindObjectsSortMode.None);
+#elif UNITY_2020_1_OR_NEWER
+            return FindObjectsOfType<Transform>(includeInactive);
 #else
-            if (includeInactive)
+            if (!includeInactive)
+                return FindObjectsOfType<Transform>();
+
+            var all = Resources.FindObjectsOfTypeAll(typeof(Transform));
+            var results = new List<Transform>(all.Length);
+
+            for (var i = 0; i < all.Length; i++)
             {
-                return Resources.FindObjectsOfTypeAll<GameObject>().Cast<GameObject>().ToArray();
+                var t = all[i] as Transform;
+                if (t == null)
+                    continue;
+
+                var go = t.gameObject;
+                if (go == null)
+                    continue;
+
+                if (!go.scene.IsValid())
+                    continue;
+
+                results.Add(t);
             }
-            else
-            {
-                return Object.FindObjectsOfType<GameObject>();
-            }
+
+            return results.ToArray();
 #endif
         }
 
-
         private static void FindMissingScriptsInScene()
         {
-            var objects = FindGameObjects();
-            var missingCount = 0;
+            var transforms = FindSceneTransforms(true);
+            var missingGameObjects = 0;
+            var missingComponents = 0;
 
-            foreach (var go in objects)
+            foreach (var t in transforms)
             {
+                if (t == null)
+                    continue;
+
+                var go = t.gameObject;
+                if (go == null || !go.scene.IsValid())
+                    continue;
+
                 var components = go.GetComponents<Component>();
+                var missingInGo = 0;
+
                 foreach (var component in components)
                 {
                     if (component == null)
-                    {
-                        missingCount++;
-                        Debug.Log($"<color=yellow>Missing script found in GameObject: {GetFullPath(go)}</color>", go);
-                    }
+                        missingInGo++;
                 }
+
+                if (missingInGo <= 0)
+                    continue;
+
+                missingGameObjects++;
+                missingComponents += missingInGo;
+
+                Debug.Log($"<color=yellow>Missing scripts: {missingInGo} in GameObject: {GetFullPath(go)}</color>", go);
             }
 
-            Debug.Log(missingCount == 0
-                ? "No missing scripts found in the scene."
-                : $"<color=magenta>Found {missingCount} GameObjects with missing scripts in the scene.</color>");
+            if (missingComponents == 0)
+            {
+                Debug.Log("No missing scripts found in the scene.");
+                return;
+            }
+
+            Debug.Log($"<color=magenta>Found {missingComponents} missing scripts on {missingGameObjects} GameObjects in the scene.</color>");
         }
 
         private static void DeleteAllMissingScriptsInScene()
         {
-            var objects = FindGameObjects();
+            var transforms = FindSceneTransforms(true);
             var removedCount = 0;
 
-            foreach (var go in objects)
+            foreach (var t in transforms)
             {
+                if (t == null)
+                    continue;
+
+                var go = t.gameObject;
+                if (go == null || !go.scene.IsValid())
+                    continue;
+
                 var components = go.GetComponents<Component>();
+                var missingInGo = 0;
+
                 foreach (var component in components)
                 {
                     if (component == null)
-                    {
-                        GameObjectUtility.RemoveMonoBehavioursWithMissingScript(go);
-                        removedCount++;
-                    }
+                        missingInGo++;
                 }
+
+                if (missingInGo <= 0)
+                    continue;
+
+                GameObjectUtility.RemoveMonoBehavioursWithMissingScript(go);
+                removedCount += missingInGo;
             }
 
-            Debug.Log(removedCount == 0
-                ? "No missing scripts found to delete in the scene."
-                : $"<color=magenta>Deleted {removedCount} missing scripts in the scene.</color>");
+            if (removedCount == 0)
+            {
+                Debug.Log("No missing scripts found to delete in the scene.");
+                return;
+            }
 
+            Debug.Log($"<color=magenta>Deleted {removedCount} missing scripts in the scene.</color>");
             EditorSceneManager.MarkAllScenesDirty();
         }
 
         private static void FindMissingScriptsInPrefabs()
         {
-            var prefabGUIDs = AssetDatabase.FindAssets("t:Prefab");
-            var missingCount = 0;
+            var prefabGuids = AssetDatabase.FindAssets("t:Prefab");
+            var missingPrefabs = 0;
+            var missingComponents = 0;
 
-            foreach (var guid in prefabGUIDs)
+            foreach (var guid in prefabGuids)
             {
                 var path = AssetDatabase.GUIDToAssetPath(guid);
                 var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+
+                if (prefab == null)
+                    continue;
+
                 var components = prefab.GetComponentsInChildren<Component>(true);
+                var missingInPrefab = 0;
 
                 foreach (var component in components)
                 {
                     if (component == null)
-                    {
-                        missingCount++;
-                        Debug.Log($"<color=yellow>Missing script found in Prefab: {path}</color>", prefab);
-                    }
+                        missingInPrefab++;
                 }
+
+                if (missingInPrefab <= 0)
+                    continue;
+
+                missingPrefabs++;
+                missingComponents += missingInPrefab;
+
+                Debug.Log($"<color=yellow>Missing scripts: {missingInPrefab} in Prefab: {path}</color>", prefab);
             }
 
-            Debug.Log(missingCount == 0
-                ? "No missing scripts found in any prefabs."
-                : $"<color=magenta>Found {missingCount} prefabs with missing scripts.</color>");
+            if (missingComponents == 0)
+            {
+                Debug.Log("No missing scripts found in any prefabs.");
+                return;
+            }
+
+            Debug.Log($"<color=magenta>Found {missingComponents} missing scripts across {missingPrefabs} prefabs.</color>");
         }
 
         private static string GetFullPath(GameObject go)
         {
             var path = "/" + go.name;
+
             while (go.transform.parent != null)
             {
                 go = go.transform.parent.gameObject;
@@ -148,3 +211,4 @@ namespace AbyssMoth
         }
     }
 }
+#endif
